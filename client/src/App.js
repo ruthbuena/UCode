a92caed72c86a7480d5ae6a85d68adc1eba42819
Renzo Renteria <Renzo@Renzos-MacBook-Pro.local>
2017-10-13 16:29:00 -0400

    components

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HTML from './components/HTML';
+import CSS from './components/CSS';
+import JavaScript from './components/Javascript';
+import ReactJs from './components/ReactJs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Welcome to YouCode React</h2>
+        </div>
+        <HTML />
+        <CSS />
+        <JavaScript />
+        <ReactJs />
       </div>
     );
   }
