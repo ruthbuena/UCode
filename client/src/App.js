5bb4cbc790d2d464b20966e7052d823b2e4f2f32
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-24 11:01:32 -0400

    auth testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,36 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactDom from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes.js';
 
-import BostonCode from './components/BostonCode/BostonCode';
-import Home from './components/Home';
+// remove tap delay, essential for MaterialUI to work properly with plugin
+injectTapEventPlugin();
+
+ReactDom.render((
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <Router history={browserHistory} routes={routes} />
+  </MuiThemeProvider>), document.getElementById('react-app'));
 
 
-const App = () =>
 
-<Router>
-	<Switch>
-	  <Route exact path="/" component = {Home} />
-      <Route exact path="/BostonCode" component= {BostonCode} />
-    </Switch>
-</Router>;
-
-
-export default App;
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//
+// import BostonCode from './components/BostonCode/BostonCode';
+// import Home from './components/Home';
+//
+//
+// const App = () =>
+//
+// <Router>
+// 	<Switch>
+// 	  <Route exact path="/" component = {Home} />
+//       <Route exact path="/BostonCode" component= {BostonCode} />
+//     </Switch>
+// </Router>;
+//
+//
+// export default App;
