8d7bef57735469c104e873e77defe8ea8dff176a
ruthbuena <ruthbpeace@gmail.com>
2017-10-24 22:31:21 -0400

    Building for production

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Col, Row, Navbar, NavItem, Button} from 'react-materialize';
 
 
-const Header = () => 
+const Header = () =>
 
 <div>
   <Row>
     <Col s={12}>
-      <Navbar className='blue-grey darken-1' brand='UCode' right>
+      <Navbar className='blue-grey darken-1' brand='HI I"M DIFFERENT NOW' right>
       <Button>Log Out</Button>
       </Navbar>
     </Col>
