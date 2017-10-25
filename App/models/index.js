689599107aef60eccd605e87e741f69c21a1f767
ruthbuena <ruthbpeace@gmail.com>
2017-10-25 01:07:49 -0400

    Building for production

--- App/models/index.js
+++ App/models/index.js
@@ -5,7 +5,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(operatorAliases: false,config.database, config.username, config.password, config);
+var sequelize = new Sequelize(config.database, config.username, config.password, config, {operatorAliases: false,});
 var db = {};
 
 
