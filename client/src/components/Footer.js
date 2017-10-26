d31f83b02fe21d5cf186fdc88b001c20f7e49128
Lebeza Wendemsesha <BezMack@Lebezas-MacBook-Pro.local>
2017-10-26 19:38:33 -0400

    final

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,9 +3,8 @@ import {Footer} from 'react-materialize';
 
 const MyFooter = () =>
 
-<Footer copyrights="UCode Copyright 2017"
-	className='example blue-grey darken-1'>
-		
+<Footer copyrights="UCode Copyright 2017" >
+
 </Footer>;
 
 export default MyFooter;
