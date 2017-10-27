3d103b49e676f9198fb9ee944b5707af0eca1abd
ruthbuena <ruthbpeace@gmail.com>
2017-10-26 21:09:41 -0400

    Building for production

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Welcome from './welcome';
 
 import Header from './Header';
-import Footer from './Footer';
+
 
 const Home = () =>
 
 	<div>
 		<mainHeader />
 		<Welcome />
-		<Footer />
 	</div>
 
 export default Home;
